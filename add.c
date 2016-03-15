85bf7a043ead7c931fcac34e55dd8b972a0656c7
Rui Wang <wangruithu@gmail.com>
2016-03-16 07:15:52 +0800

    first commit

---
+++ add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{
+	int a,b,c;
+	scanf("please input a:%d",&a);
+	scanf("please input b:%d",&b);
+	c=a+b;
+	printf("a add b is %d \n",c);
+}
---
+++ helloworld.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a=6;
+	int b=0;
+	scanf("%d",&b);
+	printf("hello world\n");
+	return 0;
+}
